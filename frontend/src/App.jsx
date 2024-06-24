09d7c3c6891123c0fdceb0962fe807b5c9a6637a
singh-moni <moni.rinki@gmail.com>
2024-06-24 11:37:49 -0400

    Components and Pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 
 import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import  CreateBooks from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook.jsx';
+import EditBook from './pages/EditBook.jsx';
+import DeleteBook from './pages/DeleteBook.jsx';
+
+
+
+
 const App = ()=> {
-  return (
+  return ( 
+    <>
+    <div classname='bg-red-400 text-white'>BookStore</div>  
    <Routes>
-    <Route path='' element={} />
-    <Route path='' element={} />
-    <Route path='' element={} />
-    <Route path='' element={} />
-    <Route path='' element={} />
+    <Route path='/' element={<Home/>} />
+    <Route path='/books/create' element={<CreateBooks/>} />
+    <Route path='/books/details/:id' element={<ShowBook/>} />
+    <Route path='./books/edit/:id' element={<EditBook/>} />
+    <Route path='/books/delete/:id' element={<DeleteBook/>} />
    </Routes>
+   </> 
   )
 }
 
---
+++ frontend/src/pages/ShowBook.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ShowBook = ()=>{
+    return (
+        <div>ShowBook</div>
+    )
+}
+export default ShowBook
