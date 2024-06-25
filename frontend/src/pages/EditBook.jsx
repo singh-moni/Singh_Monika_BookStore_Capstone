1ad71a3e56d9091f4de93c043cffb93340573b9b
singh-moni <moni.rinki@gmail.com>
2024-06-25 13:54:09 -0400

    link corrected

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -16,7 +16,7 @@ const EditBook = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`http://localhost:5555/books/${id}`)
+    axios.get(`https://singh-monika-bookstore-capstone-2.onrender.com/books/${id}`)
     .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear)
